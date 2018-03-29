82e4199e0288330567a91e7bd8f9f3c802d8f9a9
kamalkumar <kamalkumariospjt@gmail.com>
2018-03-29 11:53:19 +0530

    added new folder for Login and added storyboard for login

---
+++ RHT/RHTUITests/RHTUITests.swift
@@ -0,0 +1,36 @@
+//
+//  RHTUITests.swift
+//  RHTUITests
+//
+//  Created by vinoth on 29/03/18.
+//  Copyright © 2018 rht. All rights reserved.
+//
+
+import XCTest
+
+class RHTUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
