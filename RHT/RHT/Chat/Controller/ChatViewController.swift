4fb4c03ac40c519f44558c17dd53f9be5563af09
kamalkumar <kamal.kumar@cmlmediasoft.com>
2018-05-09 19:25:54 +0530

    Added new viewcontroller for Chat

---
+++ RHT/RHT/Chat/Controller/ChatViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChatViewController.swift
+//  RHT
+//
+//  Created by kamal on 5/9/18.
+//  Copyright © 2018 rht. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
